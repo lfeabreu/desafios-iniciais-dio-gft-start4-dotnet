bf71e6d4a075b71ec1cc9fb14ee2da3225177759
Luís Filipe Esperança de Abreu <lfeabreu@gmail.com>
2022-02-23 19:19:54 -0300

    Initial commit to Desafio3

---
+++ Desafio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Desafio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            while(x > 0)
+            {
+                //se x for impar, incrementar 1
+                if((x % 2) == 1)
+                {
+                    x++;
+                }
+                int soma5ParesSeguintes = 0;
+                for( int i = 0 ; i < 5 ; i++ )
+                {
+                    soma5ParesSeguintes = soma5ParesSeguintes + (x + (2 * i));
+                }
+                Console.WriteLine($"{soma5ParesSeguintes}");
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
